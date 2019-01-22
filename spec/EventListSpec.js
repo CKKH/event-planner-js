d9fc5f4a055a260fa7a191ce83fc926b4247189b
CKKH <chriskkhassan@gmail.com>
2019-01-22 14:27:03 +0000

    Renamed Event to EventList, made EventList function
    Co-authored-by: beacourage <44494788+beacourage@users.noreply.github.com>

--- spec/EventListSpec.js
+++ spec/EventListSpec.js
@@ -1,6 +1,6 @@
-describe('Event', () => {
+describe('EventList', () => {
   it('contains information about the event', () => {
-    var eventOne = new Event('Hello, Bea!')
+    var eventOne = new EventList('Hello, Bea!')
     expect(eventOne.information()).toEqual('Hello, Bea!')
   })
 })
--- spec/bcTestingFramework.js
+++ spec/bcTestingFramework.js
@@ -1,22 +1,11 @@
-"use strict"
+'use strict'
+
 const describe = (desc, fn) => {
   console.log(desc)
   fn()
 }
 
 const it = (msg, fn) => describe('  ' + msg, fn)
-
-var assertion;
-
-function ExpectEquality(actual, expected) {
-  if (actual === expected) {
-    console.log('%c pass', 'color:#008000');
-    return true;
-  } else {
-    console.log('%c fail', 'color:#ff0000'	);
-    return false;
-  };
-};
 
 const expect = (actual) => ({
   toEqual: (expected) => {
@@ -29,3 +18,13 @@ const expect = (actual) => ({
     }
   }
 })
+
+function ExpectEquality(actual, expected) {
+  if (actual === expected) {
+    console.log('%c pass', 'color:#008000')
+    return true;
+  } else {
+    console.log('%c fail', 'color:#ff0000')
+    return false;
+  }
+}
