21fdf5c672415f32af7e54b79263bf54e05b627a
CKKH <chriskkhassan@gmail.com>
2019-01-22 12:33:58 +0000

    Included bcTestingFramework.js
    Co-authored-by: beacourage <44494788+beacourage@users.noreply.github.com>

---
+++ spec/bcTestingFramework.js
@@ -0,0 +1,31 @@
+"use strict"
+const describe = (desc, fn) => {
+  console.log(desc)
+  fn()
+}
+
+const it = (msg, fn) => describe('  ' + msg, fn)
+
+var assertion;
+
+function ExpectEquality(actual, expected) {
+  if (actual === expected) {
+    console.log('%c pass', 'color:#008000');
+    return true;
+  } else {
+    console.log('%c fail', 'color:#ff0000'	);
+    return false;
+  };
+};
+
+const expect = (actual) => ({
+  toEqual: (expected) => {
+    if (actual === expected) {
+      console.log('%c pass', 'color:#008000')
+      return true
+    } else {
+      console.log('%c fail', 'color:#ff0000')
+      return false
+    }
+  }
+})
