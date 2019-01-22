52c49aa5246a18c323979fc121c9cc1714971ab0
beatricecourage <bea_courage@hotmail.co.uk>
2019-01-22 16:05:09 +0000

    Added a dateAndTimeOfEvent method, test passes
    Co-authored-by: CKKH <19429406+CKKH@users.noreply.github.com>

--- spec/EventListSpec.js
+++ spec/EventListSpec.js
@@ -1,6 +1,11 @@
 describe('EventList', () => {
   it('contains information about the event', () => {
-    var eventOne = new EventList('Hello, Bea!')
+    var eventOne = new EventList('Hello, Bea!','19:30, 12/02/2019' )
     expect(eventOne.information()).toEqual('Hello, Bea!')
-  })
+  });
+
+  it('contains the date the event will take place', function() {
+    var eventOne = new EventList('Hello, Chris!','19:30, 12/02/2019')
+    expect(eventOne.dateAndTimeOfEvent()).toEqual('19:30, 12/02/2019')
+  });
 })
--- src/EventList.js
+++ src/EventList.js
@@ -1,9 +1,14 @@
-function EventList (details) {
+function EventList (details, dateTime) {
   this.details = details
+  this.dateTime = dateTime
 }
 
 var eventlist;
 
 EventList.prototype.information = function() {
   return this.details
+}
+
+EventList.prototype.dateAndTimeOfEvent = function() {
+  return this.dateTime
 }
