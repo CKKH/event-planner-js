901f3bd4ff40ef4c828557535efdb61092bdf38d
CKKH <chriskkhassan@gmail.com>
2019-01-23 14:17:03 +0000

    Refactored FrameListing unit tests

--- spec/EventListSpec.js
+++ spec/EventListSpec.js
@@ -4,4 +4,10 @@ describe('EventList', () => {
     console.log(eventList)
     expect(eventList.entries.length).toEqual(0)
   })
+
+  // it('EvenListings can be stored in EventList', () => {
+  //   var eventList = new EventList()
+  //   var eventListing = new EventListing('Hello, human', '13/02/2019', '20:00')
+  //   expect(eventList.entries.length).toEqual(0)
+  // })
 })
