2291d0e0d6f9868b10f1890233b34b43a99a6831
CKKH <chriskkhassan@gmail.com>
2019-01-24 22:12:10 +0000

    Test passing: .sortUpcomingEvents returns events in chronological order

--- spec/EventListSpec.js
+++ spec/EventListSpec.js
@@ -1,6 +1,7 @@
 describe('EventList', () => {
-  let futureListing = {_details:'Future listing', _date: '02/02/2022', _time: '02:02'}
-  // let futureListingTwo = {details: 'Future listing Two', date: '03/03/2019', time: '03:03')}
+  let futureListingOne = {_details:'Future listing One', _date: '02/02/2022', _time: '02:02'}
+  let futureListingTwo = {_details: 'Future listing 2', _date: '03/03/2033', _time: '03:03'}
+  let futureListingThree = {_details: 'Future listing 3', _date: '04/04/2044', _time: '04:04'}
   let pastListing = {_details: 'Past listing', _date: '01/01/2011', _time: '01:10'}
 
   it('instantiates with an empty array', () => {
@@ -10,9 +11,29 @@ describe('EventList', () => {
 
   it('Listings can be stored in EventList', () => {
     let eventList = new EventList()
-    eventList.saveListing(futureListing)
-    expect(eventList._entries[0]).toEqual(futureListing)
+    eventList.saveListing(futureListingOne)
+    expect(eventList._entries[0]).toEqual(futureListingOne)
   })
+
+  it('.upcoming returns an array of future listings', () => {
+    let eventList = new EventList()
+    eventList.saveListing(pastListing)
+    eventList.saveListing(futureListingOne)
+    let upcomingEvents = eventList.upcoming()
+    expect(upcomingEvents[0]._date).toEqual('02/02/2022')
+  })
+
+  it('.sortUpcomingEvents returns events in chronological order', () => {
+    let eventList = new EventList()
+    eventList.saveListing(futureListingThree)
+    eventList.saveListing(futureListingOne)
+    eventList.saveListing(futureListingTwo)
+    let sortedEvents = eventList.sortUpcoming()
+    expect(sortedEvents[0]).toEqual(futureListingOne)
+    expect(sortedEvents[1]).toEqual(futureListingTwo)
+    expect(sortedEvents[2]).toEqual(futureListingThree)
+  })
+})
 
   // it('Event List can be displayed', () => {
   //   let eventList = new EventList()
@@ -21,12 +42,3 @@ describe('EventList', () => {
   //   let eventDisplay = eventList.render()
   //   expect(eventDisplay).toEqual(eventList) 
   // })
-
-  it('.upcoming returns an array of future listings', () => {
-    let eventList = new EventList()
-    eventList.saveListing(pastListing)
-    eventList.saveListing(futureListing)
-    let upcomingEvents = eventList.upcoming()
-    expect(upcomingEvents[0]._date).toEqual('02/02/2022')
-  })
-})
--- src/EventList.js
+++ src/EventList.js
@@ -13,18 +13,27 @@ EventList.prototype.upcoming = function () {
   events.forEach(function(listing) {
     let listingDate = new Date(listing._date)
     if (listingDate > today) {
-    console.log(listingDate)
       upcomingEvents.push(listing)
     }
   })
   return upcomingEvents
 }
 
+EventList.prototype.sortUpcoming = function () {
+  let sortedUpcomingEvents = this.upcoming()
+  sortedUpcomingEvents.sort((listingOne, listingTwo) => {
+    let listingOneDate = new Date(listingOne._date)
+    let listingTwoDate = new Date(listingTwo._date)
+    return listingOneDate - listingTwoDate
+  })
+  return sortedUpcomingEvents
+}
+
 // EventList.prototype.render = function () {
-//   let div = document.createElement('div');
+//   let div = document.createElement('div')
 //     this.upcoming().forEach((listing) => {
-//     let listingHtml = listing.render();
-//        div.appendChild(listingHtml);
+//     let listingHtml = listing.render()
+//        div.appendChild(listingHtml)
 //    })
 //    return div
 // }
