c00fbf7bdd3a24ab36b2b2f6fde2c6c4862a7df0
CKKH <chriskkhassan@gmail.com>
2019-01-24 12:49:03 +0000

    Refactored test checking EventList._entries is instance of Array

--- spec/EventListSpec.js
+++ spec/EventListSpec.js
@@ -1,9 +1,10 @@
 describe('EventList', () => {
   let futureListing = new Listing('Future listing', '30/02/2019', '20:00')
+  let futureListingTwo = new Listing('Future listing two', '31/03/2020', '08:00')
 
   it('instantiates with an empty array', () => {
     let eventList = new EventList()
-    expect(eventList._entries.length).toEqual(0)
+    expect(eventList._entries instanceof Array).toEqual(true)
   })
 
   it('Listings can be stored in EventList', () => {
@@ -12,7 +13,15 @@ describe('EventList', () => {
     expect(eventList._entries[0]).toEqual(futureListing)
   })
 
-  it('Upcoming events can be displayed', () => {
+  // it('Event List can be displayed', () => {
+  //   eventList = new EventList()
+  //   eventList.saveListing(futureListing)
+  //   eventList.saveListing(futureListingTwo)
+  //   let eventDisplay = eventList.render()
+  //   expect(eventDisplay).toEqual(futureListing, futureListingTwo) 
+  // })
+
+  it('Upcoming events can grouped', () => {
     let eventList = new EventList()
     let pastListing = new Listing('Past listing', '01/01/2010', '08:00')
     eventList.saveListing(pastListing)
